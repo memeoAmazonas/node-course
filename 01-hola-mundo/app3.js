72f7c26a09318ff42fa3fac388371172b72d3790
memeoAmazonas <proyectosjgot@gmail.com>
2021-03-05 19:23:36 -0400

    commit

--- 01-hola-mundo/app3.js
+++
@@ -1,6 +0,0 @@
-console.log("inicio");
-setTimeout(()=>{console.log("primer timeout") }, 3000);
-setTimeout(()=>{console.log("segundo timeout") }, 0);
-setTimeout(()=>{console.log("tercero timeout") }, 0);
-console.log("fin");
-console.log("diso")
