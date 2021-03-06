cfb069c0c69710187853dca04ef2f51a21797a10
memeoAmazonas <proyectosjgot@gmail.com>
2021-03-06 15:48:27 -0400

    commit

--- 03-bases-node/get-parameter-from-console/app.js
+++
@@ -1,7 +0,0 @@
-const { writeAsync }  =require('../files/write-file/write-file')
-const[,,arg="base=5"] = process.argv;
-const[,base] = arg.split("=");
-console.log(base);
-writeAsync(base)
-.then(file => console.log(`arhivo ${file} creado`))
-.catch(e=> console.log(e));
--- src/app.js
+++ src/app.js
@@ -1,35 +1,35 @@
 const hbs = require('hbs');
 require('dotenv').config();
 const express = require('express');
-const appExpressHandlebars = express();
+const app = express();
 const port = process.env.PORT;
 
 //handlebars
-appExpressHandlebars.set('view engine', 'hbs');
+app.set('view engine', 'hbs');
 hbs.registerPartials(__dirname + '/views/partials');
-appExpressHandlebars.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
-appExpressHandlebars.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.render(__dirname + '/views/home', {
         name: "Jose ortiz",
         title: "Pagina oficial",
         title_page: "home"
     });
 });
-appExpressHandlebars.get('/generic', (req, res) => {
+app.get('/generic', (req, res) => {
     res.render(__dirname + '/views/generic', {
         name: "Jose ortiz",
         title: "Pagina oficial",
         title_page: "generic"
     });
 });
-appExpressHandlebars.get('/elements', (req, res) => {
+app.get('/elements', (req, res) => {
     res.render(__dirname + '/views/elements', {
         name: "Jose ortiz",
         title: "Pagina oficial",
         title_page: "elements"
     });
 });
-appExpressHandlebars.get('*', (req, res) => res.render(__dirname + '/views/404'));
+app.get('*', (req, res) => res.render(__dirname + '/views/404'));
 
-appExpressHandlebars.listen(port, () => console.log("se esta ejecutando en el puerto ", port));
+app.listen(port, () => console.log("se esta ejecutando en el puerto ", port));
