2433e70897da87aacf2daa3e09956fc26c5c86cf
memeoAmazonas <proyectosjgot@gmail.com>
2021-03-06 15:53:28 -0400

    commit

--- app.js
+++ app.js
@@ -6,30 +6,30 @@ const port = process.env.PORT;
 
 //handlebars
 app.set('view engine', 'hbs');
-hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerPartials(__dirname + '/src/views/partials');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-    res.render(__dirname + '/views/home', {
+    res.render(__dirname + '/src/views/home', {
         name: "Jose ortiz",
         title: "Pagina oficial",
         title_page: "home"
     });
 });
 app.get('/generic', (req, res) => {
-    res.render(__dirname + '/views/generic', {
+    res.render(__dirname + '/src/views/generic', {
         name: "Jose ortiz",
         title: "Pagina oficial",
         title_page: "generic"
     });
 });
 app.get('/elements', (req, res) => {
-    res.render(__dirname + '/views/elements', {
+    res.render(__dirname + 'src/views/elements', {
         name: "Jose ortiz",
         title: "Pagina oficial",
         title_page: "elements"
     });
 });
-app.get('*', (req, res) => res.render(__dirname + '/views/404'));
+app.get('*', (req, res) => res.render(__dirname + '/src/views/404'));
 
 app.listen(port, () => console.log("se esta ejecutando en el puerto ", port));
